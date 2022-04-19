f8f35df419cf8dab6e1b6e6531ae336cbf65d99e
dialuponline <dialuponline@proton.me>
2022-04-19 07:46:12 +0000

    another commit

---
+++ stories/PlugConnect.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import Button, { DefaultButtonText } from '../src';
+
+const meta: Meta = {
+  title: 'Plug',
+  component:
