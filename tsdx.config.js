3f129cbd907699fcef565c215fd9441d5bf99338
dialuponline <dialuponline@proton.me>
2022-04-21 08:07:00 +0000

    New translations en_US.json (Russian)

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,2 +1,14 @@
 const url = require('@rollup/plugin-url');
-const
+const svgr = require('@svgr/rollup').default;
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins = [
+      url(),
+      svgr({
+        ref: true,
+        memo: true,
+        svgoConfig: {
+          plugins: [
+            { removeViewBox: false },
+            { removeAttrs: { attrs: 'g:(stroke|fil
